8116be3cec4d18818af14fbad5654da37aad96e3
kevinssheva <kevinssheva@gmail.com>
2023-06-04 00:32:39 +0700

    halfway in sidebar
    only added home profile notification and logout and also with the logo

---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,47 @@
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { BiLogOut } from "react-icons/bi";
+import { FaUser } from "react-icons/fa";
+import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
+
+const Sidebar = () => {
+  const items = [
+    {
+      label: "Home",
+      href: "/",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notifications",
+      href: "/notifications",
+      icon: BsBellFill,
+    },
+    {
+      label: "Profile",
+      href: "/users/123",
+      icon: FaUser,
+    },
+  ];
+
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex-col items-end flex">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              label={item.label}
+              href={item.href}
+              icon={item.icon}
+            />
+          ))}
+
+          <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
